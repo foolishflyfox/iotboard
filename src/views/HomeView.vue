b21bce4b34760403e5f346c3b79f5bb62254c0df
foolishflyfox <fenghuabin1992@163.com>
2024-10-15 08:48:02 +0800

    feat: 引入 AutoImport

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const count = ref(0);
+
+function increment() {
+  count.value += 1;
+}
+</script>
 
 <template>
   <main>
     <h1 class="text-green-600">XXXX</h1>
+    <div>count = {{ count }}</div>
+    <n-button @click="increment">++</n-button>
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,17 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { codeInspectorPlugin } from 'code-inspector-plugin';
 import UnoCss from 'unocss/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: 'src/typings/auto-import.d.ts',
+    }),
     codeInspectorPlugin({ bundler: 'vite' }),
     UnoCss(),
   ],
