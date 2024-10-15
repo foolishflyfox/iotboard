6281a3de2a633a05cc2e9afda9357f320958d60f
foolishflyfox <fenghuabin1992@163.com>
2024-10-15 11:31:38 +0800

    feat: 引入leafer库

--- src/views/mimic/WorkSpace/canvas-container/index.vue
+++ src/views/mimic/WorkSpace/canvas-container/index.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="bg-yellow-100"></div>
+  <div class="bg-yellow-100" id="mimicCanvasContainer"></div>
 </template>
 
 <script setup lang="ts">
+import { Leafer, Rect } from 'leafer-ui';
+
 defineOptions({
   name: 'CanvasContainer',
+});
+
+onMounted(() => {
+  const leafer = new Leafer({ view: 'mimicCanvasContainer' });
+
+  const rect = new Rect({
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200,
+    fill: '#32cd79',
+    cornerRadius: [50, 80, 0, 80],
+    draggable: true,
+  });
+
+  leafer.add(rect);
 });
 </script>
 
