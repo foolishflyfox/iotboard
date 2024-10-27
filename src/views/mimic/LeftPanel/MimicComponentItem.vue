b961480e00751a834bbbbe32c349c9808f8bd280
foolishflyfox <fenghuabin1992@163.com>
2024-10-27 11:18:41 +0800

    feat: 设置默认图片

--- src/views/mimic/LeftPanel/MimicComponentItem.vue
+++ src/views/mimic/LeftPanel/MimicComponentItem.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="inline-block bg-[#fff8] rounded-lg">
     <NImage
-      :src="`/preview/component/${customMeta.name}.png`"
+      :src="`${componentPath}/${customMeta.name}.png`"
       preview-disabled
-      :width="60"
-      class="cursor-grab"
+      :width="55"
+      class="cursor-grab p-4px"
+      :fallback-src="`${componentPath}/default.png`"
     />
     <div class="text-12px text-[#555] font-medium text-center cursor-default">
       {{ customMeta.label }}
@@ -18,6 +19,8 @@ import type { CustomMeta } from '../utils';
 defineProps<{
   customMeta: CustomMeta;
 }>();
+
+const componentPath = '/preview/component';
 </script>
 
 <style scoped></style>
