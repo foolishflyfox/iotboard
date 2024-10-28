5e0d2316ad1cc78dc99b2ab36fc1f5630c692bff
foolishflyfox <fenghuabin1992@163.com>
2024-10-28 10:47:49 +0800

    feat: 添加事件属性面板的占位ui

--- src/views/mimic/RightPanel/index.vue
+++ src/views/mimic/RightPanel/index.vue
@@ -16,6 +16,11 @@
         <PropertyPanel />
       </n-tab-pane>
       <n-tab-pane name="data" tab="数据"> 数据配置 </n-tab-pane>
+      <n-tab-pane name="event" tab="事件">
+        <div>
+          对于带控制/跳转的组件，存在该panel，纯显式类的组件不能配置事件
+        </div>
+      </n-tab-pane>
     </n-tabs>
   </div>
 </template>
