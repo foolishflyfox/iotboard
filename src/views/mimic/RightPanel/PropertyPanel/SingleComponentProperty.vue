cc83377f22ffcea085fa0ed6500ad8814a0e2209
foolishflyfox <fenghuabin1992@163.com>
2024-10-28 22:03:51 +0800

    feat: 添加 height 配置

--- src/views/mimic/RightPanel/PropertyPanel/SingleComponentProperty.vue
+++ src/views/mimic/RightPanel/PropertyPanel/SingleComponentProperty.vue
@@ -2,8 +2,6 @@
   <!-- <div>单文件配置: {{ mimicWorkspaceStatus.selectedUiId }}</div>
   <div>{{ curAppearancePropertyTypes }}</div> -->
   <div>
-    <!-- <InputNumberProperty v-model="y" label="Y坐标" /> -->
-    <!-- <input type="number" v-model="y" @keyup="() => {}" /> -->
     <template v-for="pt of curAppearancePropertyTypes" :key="pt">
       <template v-if="pt === 'x'">
         <InputNumberProperty v-model="x" label="X坐标" />
@@ -13,6 +11,9 @@
       </template>
       <template v-else-if="pt === 'width'">
         <InputNumberProperty v-model="width" label="宽度" />
+      </template>
+      <template v-else-if="pt === 'height'">
+        <InputNumberProperty v-model="height" label="高度" />
       </template>
     </template>
   </div>
@@ -45,6 +46,10 @@ const y = computed({
 const width = computed({
   get: () => curElementProxyData.value?.width,
   set: (v: number) => (curElementProxyData.value!.width = v),
+});
+const height = computed({
+  get: () => curElementProxyData.value?.height,
+  set: (v: number) => (curElementProxyData.value!.height = v),
 });
 </script>
 
