69c5037523bc44067cebf07d5beb743d15dcb4b6
foolishflyfox <fenghuabin1992@163.com>
2024-10-30 00:00:21 +0800

    feat: 添加帮助文档的样式

--- src/views/docs/LinearGradientDoc.vue
+++ src/views/docs/LinearGradientDoc.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>线性渐变</div>
+  <div class="blog">
+    <h1>线性渐变</h1>
+    <hr />
+    <h2>二级标题</h2>
+    <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+    <h3>三级标题</h3>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+@import './doc.css';
+</style>
