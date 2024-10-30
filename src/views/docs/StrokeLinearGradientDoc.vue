491375d301cedff8e6c271f72f1545bbabfa6e03
foolishflyfox <fenghuabin1992@163.com>
2024-10-30 18:32:05 +0800

    feat: 创建一个渐变色矩形

--- src/views/docs/StrokeLinearGradientDoc.vue
+++ src/views/docs/StrokeLinearGradientDoc.vue
@@ -12,6 +12,9 @@
       </ul>
     </div>
     <hr />
+    <div>线性渐变对象可用于描边。</div>
+    <div id="demo1" class="mt-10px h-110px"></div>
+    <div>xxx</div>
     <h2>二级标题</h2>
     <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
     <h3>三级标题</h3>
@@ -27,7 +30,30 @@
 
 <script setup lang="ts">
 import VCodeBlock from '@wdns/vue-code-block';
+import { Leafer, Rect } from 'leafer-ui';
 const code = ref(`const foo = 'bar';`);
+
+onMounted(() => {
+  const width = 100;
+  const height = 100;
+  const strokeWidth = 8;
+  const cornerRadius = 22;
+  const demo1 = new Leafer({ view: 'demo1' });
+  demo1.add(
+    new Rect({
+      width,
+      height,
+      strokeWidth,
+      cornerRadius,
+      stroke: {
+        type: 'linear',
+        from: 'top',
+        to: 'bottom',
+        stops: ['red', 'orange'],
+      },
+    }),
+  );
+});
 </script>
 
 <style scoped>
