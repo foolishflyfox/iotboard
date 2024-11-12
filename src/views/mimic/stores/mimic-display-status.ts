5af3398913ad9ff0ed8bf01856ba8eb9e8948f7c
foolishflyfox <fenghuabin1992@163.com>
2024-11-12 22:14:40 +0800

    feat: 添加图纸状态

--- src/views/mimic/stores/mimic-display-status.ts
+++ src/views/mimic/stores/mimic-display-status.ts
@@ -1,12 +1,18 @@
 import { displayBaseMapId } from '@mimic/constant';
 
+/** 图纸编辑器相关状态 */
 export const useMimicDisplayStatus = defineStore('mimic-display-status', () => {
-  // 图纸编辑器相关
   // 当前选中的 UI
   const selectedUiId = ref<null | undefined | string | string[]>();
   const selectBaseMap = () => (selectedUiId.value = displayBaseMapId);
+  // 打开的图纸路径
+  const openedDisplays = ref<string[]>([]);
+  // 当前的图纸
+  const currentDisplay = ref<string>();
   return {
     selectedUiId,
     selectBaseMap,
+    openedDisplays,
+    currentDisplay,
   };
 });
