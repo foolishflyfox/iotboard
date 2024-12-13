63469f55dfa504f43da2ff43d10d3c52bde21dfa
foolishflyfox <fenghuabin1992@163.com>
2024-12-13 13:21:25 +0800

    feat: 实现折线的流水动画

--- src/views/mimic/custom/generator/custom-line.ts
+++ src/views/mimic/custom/generator/custom-line.ts
@@ -1,18 +1,65 @@
 import type { BaseCustomCfg } from '@mimic/types';
-import { dataProcessor, Line, LineData, registerUI } from 'leafer-ui';
+import {
+  dataProcessor,
+  Line,
+  LineData,
+  registerUI,
+  surfaceType,
+  type ILineData,
+  type ILineInputData,
+} from 'leafer-ui';
 import { autoId } from '@mimic/decorates';
+import type { CustomPropertyCfgs } from './custom-ui';
 
 export interface LineCustomCfg extends BaseCustomCfg {}
+
+const FLOW_SPEED_MAX = 10;
 
 export function customLineGenerate(lineCustomCfg: LineCustomCfg) {
   interface CustomData {
     /** 线条在虚线模式下的，0 表示无流动
-     * 正数和负数表示不同的流动方向
+     * 正数表示从起始点流向结束点，负数表示从结束点流向起始点
      * 值表示流动速度，绝对值越大，流动速度越快 */
     flowSpeed?: number;
   }
 
-  class InnerData extends LineData {}
+  interface ICustomLineInputData extends ILineInputData, CustomData {}
+  interface ICustomLineData extends ILineData, CustomData {}
+
+  class InnerData extends LineData implements ICustomLineData {
+    protected _flowSpeed?: number;
+
+    private innerDashOffset: number = 0;
+    private currentTick = 0;
+
+    private renderFlow() {
+      if (this._flowSpeed) {
+        this.currentTick += this._flowSpeed < 0 ? -1 : 1;
+        if (this.currentTick < -1 * FLOW_SPEED_MAX) {
+          this.innerDashOffset += 1;
+          this.currentTick = this._flowSpeed;
+          this.__leaf.dashOffset = this.innerDashOffset;
+        } else if (this.currentTick > FLOW_SPEED_MAX) {
+          this.innerDashOffset -= 1;
+          this.currentTick = this._flowSpeed;
+          this.__leaf.dashOffset = this.innerDashOffset;
+        }
+        this.__leaf.nextRender(this.renderFlow, this);
+      }
+    }
+
+    protected setFlowSpeed(v: number) {
+      this._flowSpeed = v;
+      // this.__leaf.nextRender
+      this.__leaf.removeNextRender(this.renderFlow);
+      if (v) {
+        this.currentTick = this._flowSpeed;
+        this.__leaf.nextRender(this.renderFlow, this);
+      } else {
+        this.currentTick = 0;
+      }
+    }
+  }
 
   @registerUI()
   @autoId()
@@ -26,7 +73,11 @@ export function customLineGenerate(lineCustomCfg: LineCustomCfg) {
     @dataProcessor(InnerData)
     declare public __: any;
 
-    constructor(data: any) {
+    @surfaceType()
+    declare public flowSpeed: number;
+
+    constructor(data: ICustomLineInputData) {
+      const flowSpeed = 0;
       super({
         strokeWidth: 5,
         stroke: {
@@ -36,8 +87,27 @@ export function customLineGenerate(lineCustomCfg: LineCustomCfg) {
         startArrow: 'circle',
         endArrow: 'arrow',
         cornerRadius: 0,
+        flowSpeed,
         ...data,
       });
+      this.flowSpeed = flowSpeed;
     }
   }
+
+  let id = 1;
+  const customCfgs: CustomPropertyCfgs = [
+    {
+      id: id++,
+      group: '动画',
+      name: 'flowSpeed',
+      label: '流动速率',
+      type: 'number',
+      variable: true,
+      extra: {
+        min: -10,
+        max: 10,
+      },
+    },
+  ];
+  return customCfgs;
 }
