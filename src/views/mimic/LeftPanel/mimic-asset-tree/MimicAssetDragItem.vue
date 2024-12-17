bcd2d3747c4b921ee84944c20884b6d97e42204a
foolishflyfox <fenghuabin1992@163.com>
2024-12-18 07:28:08 +0800

    feat: 为 asset 添加类型 tag

--- src/views/mimic/LeftPanel/mimic-asset-tree/MimicAssetDragItem.vue
+++ src/views/mimic/LeftPanel/mimic-asset-tree/MimicAssetDragItem.vue
@@ -1,12 +1,22 @@
 <template>
-  <MimicItem
-    editor-type="asset"
-    :img-src
-    :draggable
-    :file-name
-    @delete="deleteAsset"
-    @drag-start="assetStartDragHandler"
-  />
+  <div>
+    <div>
+      <div
+        class="text-10px font-bold rounded-t-3px bg-[#eee] inline-block px-3px "
+        :style="{ color: typeColor }"
+      >
+        {{ fileExt }}
+      </div>
+    </div>
+    <MimicItem
+      editor-type="asset"
+      :img-src
+      :draggable
+      :file-name
+      @delete="deleteAsset"
+      @drag-start="assetStartDragHandler"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -25,6 +35,13 @@ const emit = defineEmits<{
 }>();
 
 const fileName = computed(() => path.basename(props.imgSrc));
+const fileExt = computed(() => path.extname(fileName.value).slice(1));
+const typeColor = computed(() => {
+  if (fileExt.value === 'png') return 'blue';
+  else if (fileExt.value === 'svg') return '#ea6140';
+  else if (fileExt.value === 'jpg' || fileExt.value === 'jpeg') return 'green';
+  return 'black';
+});
 
 const mimicWorkspaceStatus = useMimicWorkspaceStatus();
 const { curEditorType } = toRefs(mimicWorkspaceStatus);
