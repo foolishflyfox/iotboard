26119f80427f660d54061eee6527c162c1eac757
foolishflyfox <fenghuabin1992@163.com>
2024-12-26 10:31:31 +0800

    feat: 为操作的diff添加响应式变量

--- src/views/mimic/utils/opt-diff-manager.ts
+++ src/views/mimic/utils/opt-diff-manager.ts
@@ -8,34 +8,50 @@ export type OptDiff = OptDiffItem[];
 
 export class JsonDiffPatchManager {
   private undoPatchs: OptDiff[];
+  private undoCount: Ref<number>;
+  private undoEnable: ComputedRef<boolean>;
   private redoPatchs: OptDiff[];
+  private redoCount: Ref<number>;
+  private redoEnable: ComputedRef<boolean>;
+
   constructor() {
     this.undoPatchs = [];
+    this.undoCount = ref(this.undoPatchs.length);
+    this.undoEnable = computed(() => this.undoCount.value > 0);
     this.redoPatchs = [];
+    this.redoCount = ref(this.redoPatchs.length);
+    this.redoEnable = computed(() => this.redoCount.value > 0);
   }
 
-  undoEnable() {
-    return this.undoPatchs.length > 0;
+  private updateCount() {
+    this.undoCount.value = this.undoPatchs.length;
+    this.redoCount.value = this.redoPatchs.length;
   }
 
-  redoEnable() {
-    return this.redoPatchs.length > 0;
+  getUndoEnable() {
+    return this.undoEnable;
   }
 
-  getUndoDiff() {
+  getRedoEnable() {
+    return this.redoEnable;
+  }
+
+  undo() {
     let diff: OptDiff = [];
     if (this.undoPatchs.length > 0) {
       diff = this.undoPatchs.pop()!;
       this.redoPatchs.push(diff);
+      this.updateCount();
     }
     return diff;
   }
 
-  getRedoDiff() {
+  redo() {
     let diff: OptDiff = [];
     if (this.redoPatchs.length > 0) {
       diff = this.redoPatchs.pop()!;
       this.undoPatchs.push(diff);
+      this.updateCount();
     }
     return diff;
   }
