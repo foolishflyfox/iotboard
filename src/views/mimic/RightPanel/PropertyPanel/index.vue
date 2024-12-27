ccbc5e04ab99760111b39da67cb41aac5942ecaf
foolishflyfox <fenghuabin1992@163.com>
2024-12-27 11:03:39 +0800

    feat: 添加操作图标

--- src/views/mimic/RightPanel/PropertyPanel/index.vue
+++ src/views/mimic/RightPanel/PropertyPanel/index.vue
@@ -14,10 +14,29 @@
             <div>空</div>
           </template>
           <template v-else-if="_.isArray(selectedUI)">
-            多元素配置:
-            <div>均匀分布</div>
-            <div>左/中/右对齐</div>
-            <div>{{ selectedUI }}</div>
+            <div class="px-1em flex justify-between">
+              <NSpace>
+                <HeadIconContainer :vicons="AlignHorizontalLeftRound" tooltip="左对齐" />
+                <HeadIconContainer :vicons="AlignHorizontalCenterRound" tooltip="水平居中" />
+                <HeadIconContainer :vicons="AlignHorizontalRightRound" tooltip="右对齐" />
+              </NSpace>
+              <NSpace>
+                <HeadIconContainer :vicons="AlignVerticalTopRound" tooltip="上对齐" />
+                <HeadIconContainer :vicons="AlignVerticalCenterRound" tooltip="垂直居中" />
+                <HeadIconContainer :vicons="AlignVerticalBottomRound" tooltip="下对齐" />
+              </NSpace>
+            </div>
+            <hr class="text-#ddd my-5px">
+            <div class="px-1em flex justify-between">
+              <NSpace>
+                <HeadIconContainer :vicons="HorizontalDistributeRound" tooltip="水平等距" />
+                <HeadIconContainer :vicons="VerticalDistributeRound" tooltip="垂直等距" />
+              </NSpace>
+              <NSpace>
+                <HeadIconContainer :vicons="AutoFitWidth20Filled" tooltip="等宽" />
+                <HeadIconContainer :vicons="AutoFitHeight20Filled" tooltip="等高" />
+              </NSpace>
+            </div>
           </template>
           <template v-else-if="selectedUI?.id === displayBaseMapId">
             <DisplayProperty />
@@ -42,13 +61,25 @@
 </template>
 
 <script setup lang="ts">
-import { NSplit, NScrollbar } from 'naive-ui';
+import { NSplit, NScrollbar, NSpace, NDivider } from 'naive-ui';
 import * as _ from 'lodash-es';
 import { useMimicDisplayStatus, useMimicWorkspaceStatus } from '@mimic/stores';
 import DisplayProperty from './DisplayProperty.vue';
 import SingleComponentProperty from './SingleComponentProperty.vue';
 import { displayBaseMapId } from '@mimic/constant';
 import { mimicVar } from '@mimic/global';
+import {
+  AlignHorizontalLeftRound,
+  AlignHorizontalCenterRound,
+  AlignHorizontalRightRound,
+  AlignVerticalTopRound,
+  AlignVerticalCenterRound,
+  AlignVerticalBottomRound,
+  HorizontalDistributeRound,
+  VerticalDistributeRound,
+} from '@vicons/material';
+import { AutoFitWidth20Filled, AutoFitHeight20Filled } from '@vicons/fluent';
+import HeadIconContainer from '@mimic/components/HeadIconContainer.vue';
 
 defineOptions({
   name: 'PropertyPanel',
@@ -60,4 +91,12 @@ const { cursorPos } = toRefs(useMimicWorkspaceStatus());
 const selectedUI = computed(() => mimicVar.displayEditor.findUiById(selectedUiId.value));
 </script>
 
-<style scoped></style>
+<style scoped>
+.vertical-hr {
+  width: 1px;
+  height: 32px;
+  transform: rotate(90);
+  background-color: #ddd;
+  margin: 0 5px;
+}
+</style>
